edd0b996d7781af176c1ebb55bfb261aad694259
Deependra Shekhawat <jeevanullas@gmail.com>
2013-04-17 17:41:53 +0530

    fixed ident in driver and removed keys

--- driver.rb
+++ driver.rb
@@ -1,14 +1,14 @@
 require_relative 'walrus'
 
 conn = Walrus.new({
-		:s3_endpoint => '192.168.51.81',
-		:s3_port => 8773,
-		:s3_service_path => '/services/Walrus/',
-	 	:use_ssl => false,	
-		:s3_force_path_style => true,
-		:access_key_id => 'FVWHLUWNNXGFXSVSOTXTT',
-		:secret_access_key => 'Kk7hkLB7tZBMXy00k7SOWXbT7Lo0ZTJ60MMhvf5x',
-	   })
+         :s3_endpoint => '<Walrus IP address>',
+         :s3_port => 8773,
+         :s3_service_path => '/services/Walrus/',
+         :use_ssl => false,	
+         :s3_force_path_style => true,
+         :access_key_id => '<Access Key ID>',
+         :secret_access_key => '<Secret Key>',
+       })
 
 # Creating a bucket
 conn.create_bucket('mybucket')
